42f140ed8a350a4e9a81887c0e4b4da913164f4a
J4g0n <simon.andreux@gmail.com>
2013-05-01 18:29:43 +0200

    Ajout documentation

--- FileTest.java
+++ FileTest.java
@@ -2,12 +2,15 @@ import java.lang.*;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Classe de test d'affichage en console
+ * @author J. Pallamidessi & S. Andreux
+ */
 public class FileTest 
 {
 	public static void main(String [] args)
 	{
-		FileTree f=new FileTree("/usr",2,1);
+		FileTree f=new FileTree("/",2,1);
 		f.print();
-
 	}
 }
--- TestAffichage.java
+++ TestAffichage.java
@@ -2,17 +2,19 @@ import java.lang.*;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Classe testant l'affichage en mode graphique
+ * @author J. Pallamidessi
+ */
 public class TestAffichage 
 {
 	public static void main(String [] args)
 	{
-		FileTree f=new FileTree("/usr",2,1);
+		FileTree f=new FileTree("/",2,1);
 		WindowProjet w=new WindowProjet(f);
 		
-
 		w.afficher();
 		w.pack();
 		w.setVisible(true);
-		
 	}
 }
